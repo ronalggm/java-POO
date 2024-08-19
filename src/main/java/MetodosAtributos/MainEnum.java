d30b494cf703d21b9ee4e82f115d2613ce6b7d10
totone <ronalggm@gmail.com>
2024-08-19 20:06:44 -0300

    agregado switchCase

---
+++ src/main/java/MetodosAtributos/MainEnum.java
@@ -0,0 +1,70 @@
+package MetodosAtributos;
+
+public class MainEnum {
+    public static void main(String[] args) {
+
+
+        Automovil subaru = new Automovil();
+        subaru.setFabricante("Subaru");
+        subaru.setModelo("Impresa");
+        subaru.setCilindrada(2.0);
+        subaru.setColor(Color.NARANJA);
+        subaru.setColorMatricula(Automovil.COLOR_AMARILLO); //constante de forma clasica
+        subaru.setTipo(TipoAutomovil.COUPE);
+        System.out.println();
+
+//OBJETO DE TIPO AUTOMOVIL CON CONSTRUCTOR SIMPLE
+        Automovil mazda = new Automovil();
+        mazda.setFabricante("Mazda");
+        mazda.setModelo("rx7");
+        mazda.setCilindrada(4.2);
+        mazda.setColor(Color.BLANCO);
+        mazda.setTipo(TipoAutomovil.SEDAN);
+
+        //CON CONSTRUCTOR POR PARAMETROS
+        Automovil mercedez = new Automovil(
+                "mercedez",
+                "blk",
+                Color.AMARILLO,
+                2.4);
+        mercedez.setTipo(TipoAutomovil.SEDAN);
+
+
+        System.out.println(subaru.toString());
+        System.out.println();
+        System.out.println(mazda.toString());
+        System.out.println();
+
+        //comparando tipos de vehiculosl;
+        TipoAutomovil tipoSubaru = subaru.getTipo();
+        TipoAutomovil tipoMazda = mazda.getTipo();
+        TipoAutomovil tipoMercedez = mercedez.getTipo();
+        switch (tipoSubaru) {
+            case CONVERTIBLE:
+                System.out.println("El automovil es convertible");
+                break;
+
+            case FURGON:
+                System.out.println("El automovil es un furgon");
+                break;
+            case COUPE:
+                System.out.println("El automovil es un coupe");
+                break;
+            case HATCHBACK:
+                System.out.println("El automovil es un HatchBack");
+                break;
+
+            case PICKUP:
+                System.out.println("El automovil es un pickUp");
+                break;
+            case SEDAN:
+                System.out.println("El automovil es un Sedan");
+                break;
+            case STATION_WAGON:
+                System.out.println("El automovil es un stationWagon");
+        }
+
+    }
+
+
+}
--- src/main/java/MetodosAtributos/TipoAutomovil.java
+++ src/main/java/MetodosAtributos/TipoAutomovil.java
@@ -3,7 +3,7 @@ package MetodosAtributos;
 public enum TipoAutomovil {
     SEDAN("Sedan", "Auto mediano", 4),
     STATION_WAGON("Station Wagon", "Auto grande", 5),
-    HATCHBACK("Harchback", "Auto compacto", 4),
+    HATCHBACK("Hatchback", "Auto compacto", 4),
     PICKUP("Pickup", "Camioneta", 4),
     COUPE("Coupé", "Auto pequeño", 2),
     CONVERTIBLE("Convertible", "Auto deportivo", 2),
